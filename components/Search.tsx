856cf1c9739568cbfa201980584e39373f8b47fa
MARUNGs <mi1764@naver.com>
2024-11-13 18:06:33 +0900

    반응형 CSS

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,14 +1,28 @@
 export default function Search() {
   return (
     <>
-      <div className="bg-gray-100 h-screen p-5 flex items-center justify-center ">
-        <div className="gap-2 bg-white max-w-screen-sm w-full shadow-lg p-5 rounded-3xl flex flex-col">
+      <div
+        className="h-screen p-5 flex items-center justify-center 
+        bg-gray-100
+        sm:bg-red-100
+        md:bg-green-100
+        lg:bg-cyan-100
+        xl:bg-orange-100
+        2xl:bg-purple-100"
+      >
+        <div className="md:flex-row gap-2 bg-white max-w-screen-sm w-full shadow-lg p-5 rounded-3xl flex flex-col">
           <input
-            className="py-3 w-full rounded-full h-10 bg-gray-200 pl-5"
+            className="py-3 w-full rounded-full h-10 bg-gray-200 pl-5 outline-none
+              ring 
+              ring-transparent
+              focus:ring-orange-500 
+              focus:ring-offset-2 
+              transition-shadow
+              placeholder:drop-shadow"
             type="text"
             placeholder="Search here ..."
           />
-          <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium">
+          <button className="md:px-10 bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">
             Search
           </button>
         </div>
