5ccf7726be44e813f524920633235e77c019b20b
MARUNGs <mi1764@naver.com>
2024-11-27 18:50:00 +0900

    token 유효성 검사 및 유저 로그인 수행

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,11 +2,25 @@
 import { z } from "zod";
 import validator from "validator";
 import { MAX_NUMBER, MIN_NUMBER } from "@/lib/constants";
-import { redirect } from "next/navigation";
 import db from "@/lib/db";
 import crypto from "crypto";
+import { userLogin } from "@/lib/login";
 
 // phone 먼저 검사한 뒤에 token을 검사할 것이므로 object()로 감쌀 필요가 없다.
+
+/**
+ * 현재 token이 DB에서 unique한지 조회
+ * @param token
+ * @returns
+ */
+async function tokenExists(token: number) {
+  const exists = await db.sMSToken.findUnique({
+    where: { token: token.toString() },
+    select: { id: true },
+  });
+
+  return Boolean(exists);
+}
 
 const phoneSchema = z
   .string()
@@ -17,15 +31,23 @@ const phoneSchema = z
   );
 // input에서 number로 보내도 실제 받는 건 string이다.
 // string을 number로 변환하자.
-const tokenSchema = z.coerce.number().min(MIN_NUMBER).max(MAX_NUMBER);
+const tokenSchema = z.coerce
+  .number()
+  .min(MIN_NUMBER)
+  .max(MAX_NUMBER)
+  .refine(tokenExists, "해당 token이 존재하지 않습니다.");
 
 // prevState는 화면단에서 정의한 useFormState의 초기값이 들어가게 된다.
 interface ActionState {
   token: boolean;
 }
 
+/**
+ * Token 생성 및 가져오기(재귀함수)
+ * @returns
+ */
 async function getToken(): Promise<string> {
-  const token = crypto.randomInt(100000, 999999).toString();
+  const token = crypto.randomInt(MIN_NUMBER, MAX_NUMBER).toString();
   const exists = await db.sMSToken.findUnique({
     where: { token },
     select: { id: true },
@@ -89,7 +111,7 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
     }
   } else {
     // 전화번호가 있다면, token을 받아야 한다.
-    const result = tokenSchema.safeParse(token);
+    const result = await tokenSchema.spa(token);
     // 전화번호 유효성 검증에 실패했다면 리턴한다.
     if (!result.success) {
       return {
@@ -97,8 +119,19 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
         error: result.error.flatten(),
       };
     } else {
-      // 로그인 시켜주기
-      redirect("/");
+      // token을 갖는 userId 찾기
+      const token = await db.sMSToken.findUnique({
+        where: { token: result.data.toString() },
+        select: { id: true, userId: true },
+      });
+
+      // user 로그인 시켜주기
+      await userLogin({ id: token!.userId });
+
+      // 로그인하고 나면 더이상 token은 필요없으므로 완전삭제한다.
+      await db.sMSToken.delete({
+        where: { id: token!.id },
+      });
     }
   }
 }
