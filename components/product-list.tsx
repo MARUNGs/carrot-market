18e87f7746ed2634f75f8f7857c3285878724821
MARUNGs <mi1764@naver.com>
2024-12-01 22:30:49 +0900

    스크롤 수정

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -14,13 +14,20 @@ export default function ProductLists({ initProducts }: IProps) {
   const [products, setProducts] = useState(initProducts.data);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(0);
+  const [lastPage, setLastPage] = useState(false);
 
   const onLoadMoreClick = async () => {
     setLoading(true);
     const { data: list } = await getMoreProducts(page + 1);
-    setPage((prev) => prev + 1);
-    setProducts((prev) => [...prev, ...list]);
-    setLoading(false);
+
+    // 데이터가 존재할때만 페이지 번호를 증가시킨다.
+    if (list.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...list]);
+      setLoading(false);
+    } else {
+      setLastPage(true);
+    }
   };
 
   return (
@@ -30,17 +37,21 @@ export default function ProductLists({ initProducts }: IProps) {
           <ProductList key={product.id} {...product} />
         ))}
 
-      <button
-        onClick={onLoadMoreClick}
-        disabled={loading}
-        className={`${clsx(
-          "text-sm font-semibold",
-          "bg-orange-500 w-fit mx-auto px-3 py-2",
-          "rounded-md hover:opacity-90 active:scale-95"
-        )}`}
-      >
-        {loading ? "Loading ..." : "Load more"}
-      </button>
+      {lastPage ? (
+        "더 이상 상품이 존재하지 않습니다."
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={loading}
+          className={`${clsx(
+            "text-sm font-semibold",
+            "bg-orange-500 w-fit mx-auto px-3 py-2",
+            "rounded-md hover:opacity-90 active:scale-95"
+          )}`}
+        >
+          {loading ? "Loading ..." : "Load more"}
+        </button>
+      )}
     </div>
   );
 }
