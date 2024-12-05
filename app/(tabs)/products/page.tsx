e83e78ddffb03d9bf81fdb3777cda698c98118db
MARUNGs <mi1764@naver.com>
2024-12-05 13:08:17 +0900

    cache :: revalidate basic

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -13,7 +13,13 @@ import Link from "next/link";
 // [2] NextJS가 관리하는 cache key list 중에서 home-products가 관리하는 데이터를 찾게 된다.
 //     처음에는 데이터가 없으므로 findProductList action 함수를 이용하여 데이터를 얻게 된다.
 // [3] 이제 home-products key에 저장된 cache data를 호출하여 getCachedProducts에 담게 된다.
-const getCachedProducts = nextCache(findProductList, ["home-products"]);
+const getCachedProducts = nextCache(findProductList, ["home-products"], {
+  // cache 속성
+  // [4] findProductList 함수가 호출되는 순간부터 시간이 흐른다.
+  //     유저가 60초 이내에 데이터를 호출하면 cache에 저장된 데이터를 전달해줄 것이고
+  //     60초가 지나면 다시 findProductList 함수를 호출하여 데이터를 전달하게 된다.
+  revalidate: 60, // 60초 기준
+});
 
 export const metadata = {
   title: "Home",
